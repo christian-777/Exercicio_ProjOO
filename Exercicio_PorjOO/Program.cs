10b5b37060077668dd37c835abaee842ad12be59
christian-777 <christian.c.g.f777@hotmail.com>
2023-03-13 13:59:22 -0300

    Add project files.

---
+++ Exercicio_PorjOO/Program.cs
@@ -0,0 +1,52 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Pessoa p = new Pessoa();
+        Console.WriteLine("informe seu nome");
+        p.DefinirNome(Console.ReadLine());
+        Console.WriteLine("informe sobrenome");
+        p.DefinirSobrenome(Console.ReadLine());
+        Console.WriteLine("Nome: " + p.RetornarNome());
+        Console.WriteLine("Sobrenome: " + p.RetornarSobrenome());
+        Console.WriteLine("Nome completo: " + p.RetornarNomeCompleto());
+    }
+
+    public class Pessoa
+    {
+        string? nome;
+        string? sobrenome;
+
+        public Pessoa()
+        {
+
+        }
+
+        public void DefinirNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public string RetornarNome()
+        {
+            return this.nome;
+        }
+
+        public void DefinirSobrenome(string sobrenome)
+        {
+            this.sobrenome = sobrenome;
+        }
+
+        public string RetornarSobrenome()
+        {
+            return this.sobrenome;
+        }
+
+        public string RetornarNomeCompleto()
+        {
+
+            return this.nome + " " + this.sobrenome;
+        }
+    }
+}
+
